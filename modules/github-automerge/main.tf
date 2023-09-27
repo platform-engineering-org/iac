b40a320fd06074ad1f355337f618839e7a9ee330
Liora Milbaum <lmilbaum@redhat.com>
2023-09-27 10:24:27 +0300

    github-automerge second github org

--- modules/github-automerge/main.tf
+++ modules/github-automerge/main.tf
@@ -1,14 +1,28 @@
-data "github_repositories" "repos" {
-  query           = "org:${var.owner} archived:false"
+data "github_repositories" "repos_1" {
+  query           = "org:${var.owner_1} archived:false"
   include_repo_id = true
 }
 
-resource "github_branch_protection" "main_branch_protection" {
-  for_each = toset(data.github_repositories.repos.names)
-
+resource "github_branch_protection" "repos_1_main_branch_protection" {
+  for_each      = toset(data.github_repositories.repos_1.names)
   repository_id = each.value
   pattern       = "main"
+  required_pull_request_reviews {
+    pull_request_bypassers = ["/${var.bot_user_name}"]
+  }
+}
 
+data "github_repositories" "repos_2" {
+  provider        = github.secondary
+  query           = "org:${var.owner_2} archived:false"
+  include_repo_id = true
+}
+
+resource "github_branch_protection" "repos_2_main_branch_protection" {
+  provider      = github.secondary
+  for_each      = toset(data.github_repositories.repos_2.names)
+  repository_id = each.value
+  pattern       = "main"
   required_pull_request_reviews {
     pull_request_bypassers = ["/${var.bot_user_name}"]
   }
--- modules/github-automerge/providers.tf
+++ modules/github-automerge/providers.tf
@@ -1,4 +1,10 @@
 provider "github" {
-  owner = var.owner
+  owner = var.owner_1
+  token = var.github_token
+}
+
+provider "github" {
+  alias = "secondary"
+  owner = var.owner_2
   token = var.github_token
 }
