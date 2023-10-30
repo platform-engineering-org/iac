63702896c24a203c754eddadfd787020658ec4c0
null <platform-engineering@redhat.com>
2023-10-30 01:13:21 +0000

    Update all dependencies

--- modules/oidc/versions.tf
+++ modules/oidc/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.22.0"
+      version = "5.23.1"
     }
     http = {
       source  = "hashicorp/http"
@@ -14,7 +14,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "5.40.0"
+      version = "5.41.0"
     }
   }
 }
